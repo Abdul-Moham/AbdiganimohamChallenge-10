56f34c338736b685eded1d33789c7867f64daa87
Abdigani Mohamud <jamezwitaz2@gmail.com>
2022-06-01 19:21:53 -0400

    challenge 4 starting

---
+++ quiz.js
@@ -0,0 +1,14 @@
+var introEl= document.querySelector("#intro")
+var
+
+/* step 1. display the start page - titile and paragraph
+start button = starts the quiz game 
+
+step 2. display question page and hide start page
+when anwsered correctly show correct 
+when answered wrong. time gets deducted 
+
+step 3. once all the questions are answered display score
+
+step 4. show a dashboard of all highscores.
+*/ 
